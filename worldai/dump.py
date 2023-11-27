291d3c16bd5972bb663444c83352f7887d8b1462
jim <jmwanderer@gmail.com>
2023-11-26 17:26:15 -0800

    Add ability to dump database

---
+++ worldai/dump.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Dump the contents of the database
+"""
+
+import datetime
+import os
+import json
+
+from . import elements
+from . import chat_functions
+
+dir = os.path.split(os.path.split(__file__)[0])[0]
+dir = os.path.join(dir, 'instance')
+print(f"dir: {dir}")
+chat_functions.init_config(dir, "worldai.sqlite")
+
+print("Loading worlds...")
+worlds = elements.listWorlds(chat_functions.get_db())
+print("%d worlds listed" % len(worlds))
+
+for (entry) in worlds:
+  id = entry["world_id"]
+  name = entry["name"]
+  print(f"World({id}): {name}")
+  
+  world = elements.loadWorld(chat_functions.get_db(), id)
+  print(world.getPropertiesJSON())
+
